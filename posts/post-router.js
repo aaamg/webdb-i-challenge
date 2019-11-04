bcc1ed26bdc1e8ec8fe143449c68f38724ef464d
aaamg <andrewmanuelgarcia@gmail.com>
2019-11-04 17:34:09 -0500

    MVP

--- posts/post-router.js
+++ posts/post-router.js
@@ -23,7 +23,7 @@ router.get('/:id', (req, res) => {
     knex
         .select('*')
         .from('accounts')
-        .where(id = req.params.id)
+        .where('id', '=', req.params.id)
         .then(account => {
             res.status(200).json(account)
         })
@@ -34,14 +34,46 @@ router.get('/:id', (req, res) => {
 })
 
 router.post('/', (req, res) => {
+    knex
+        .insert(req.body, 'id') //ignore the console warning on sqlite
+        .into('accounts')
+        .then(ids => {
+            res.status(200).json(ids)
+        })
+        .catch(err => {
+            res.status(500).json({ error: "Failed to post into db"})
+        })
     
 })
 
 router.delete('/:id', (req, res) => {
+    knex('accounts')
+    .where({ id: req.params.id})
+    .del()
+    .then(count => {
+        // count: how many records/rows were deleted
+        res.status(200).json(count)
+        })
+        .catch(err => {
+            res.status(500).json({ error: "Failed to get post from db"})
+        })
     
 })
 
 router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+
+    knex('accounts')
+    .where({ id: req.params.id }) // filter results
+    .update(body)
+    .then(count => {
+        // count: how many records/rows were updated
+        res.status(200).json(count)
+        })
+        .catch(err => {
+            res.status(500).json({ error: "Failed to get post from db"})
+        })
     
 })
 
